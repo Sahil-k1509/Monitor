fe6c2cb2d59abd35557722876f48c3cb053303fe
Sahil <sahilbairagi319@gmail.com>
2020-06-10 13:56:10 +0530

    Updates

---
+++ precache-manifest.db04bfdf3fbadb15fd56e60f8027a4b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "713cb81ad341e72a1b933975c6f19fd4",
+    "url": "/Monitor/index.html"
+  },
+  {
+    "revision": "3576cd6273c76a34ba30",
+    "url": "/Monitor/static/css/main.6486fb49.chunk.css"
+  },
+  {
+    "revision": "3a0ee60e6833a5061c39",
+    "url": "/Monitor/static/js/2.d412a7af.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Monitor/static/js/2.d412a7af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3576cd6273c76a34ba30",
+    "url": "/Monitor/static/js/main.84c41024.chunk.js"
+  },
+  {
+    "revision": "63b0067f112bf9b835d5",
+    "url": "/Monitor/static/js/runtime-main.8cbbcd78.js"
+  },
+  {
+    "revision": "477170f2b0aa14e7ef356fd29268b040",
+    "url": "/Monitor/static/media/books.477170f2.jpg"
+  },
+  {
+    "revision": "e8524d5e6fe89008f2d817694dfd797a",
+    "url": "/Monitor/static/media/desk.e8524d5e.jpg"
+  }
+]);
